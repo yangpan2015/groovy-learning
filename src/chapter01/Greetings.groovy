fbcc2bb3eb5ce423297edd74ea6800eb0f449f07
yangpan <yangpan@allinpalm.com>
2020-12-22 10:57:35 +0800

    Greetings

---
+++ src/chapter01/Greetings.groovy
@@ -0,0 +1,14 @@
+package chapter01
+
+/**
+ * @author Pan Yang
+ * @since 2020/12/22 10:53
+ */
+class Greetings {
+    static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            println 'ho '
+        }
+        println 'groovy'
+    }
+}
